079962800347abad49d8d92a69562c00d0f2d2d3
Yusaku Hieda <uiyiuyiyi@gmail.com>
2023-05-27 14:56:02 +0900

    fix

--- .config/zsh/zshrc/Darwin/bindkey.zsh
+++ .config/zsh/zshrc/Darwin/bindkey.zsh
@@ -9,9 +9,3 @@ function ghq-fzf() {
 zle -N ghq-fzf
 bindkey '^]' ghq-fzf
 
-function fgm() {
-  local src=$(ghq list | fzf --preview "ls -laTp $(ghq root)/{} | tail -n+4 | awk '{print \$9\"/\"\$6\"/\"\$7 \" \" \$10}'")
-  if [ -n "$src" ]; then
-    cd $(ghq root)/$src
-  fi
-}
