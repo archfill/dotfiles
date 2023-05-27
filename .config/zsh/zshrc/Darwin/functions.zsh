13f56c51eea7631484ceedb718e99a60ce549ba4
Yusaku Hieda <uiyiuyiyi@gmail.com>
2023-05-27 14:56:02 +0900

    fix

--- .config/zsh/zshrc/Darwin/functions.zsh
+++ .config/zsh/zshrc/Darwin/functions.zsh
@@ -19,3 +19,10 @@ function gx() {
   gcloud-activate "${name}" "${project}"
 }
 compdef gx-complete gx
+
+function g() {
+  local src=$(ghq list | fzf --preview "ls -laTp $(ghq root)/{} | tail -n+4 | awk '{print \$9\"/\"\$6\"/\"\$7 \" \" \$10}'")
+  if [ -n "$src" ]; then
+    cd $(ghq root)/$src
+  fi
+}
