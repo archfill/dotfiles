1673ece7ceb0caf99ed9b4f924936a80f2a8c93c
Yusaku Hieda <uiyiuyiyi@gmail.com>
2023-05-28 23:44:35 +0900

    fix zsh config

--- .config/zsh/zshrc/Darwin/alias.zsh
+++ .config/zsh/zshrc/Darwin/alias.zsh
@@ -2,4 +2,4 @@ alias mvim=/Applications/MacVim.app/Contents/bin/mvim "$@"
 alias bau='brew update && brew upgrade && brew upgrade --cask --greedy'
 alias bwupgrade='brew update && brew upgrade'
 alias intelzsh='arch -x86_64 zsh'
-alias yabai-restart='brew services restart yabai'
+alias yabai-restart='yabai --restart-service'
