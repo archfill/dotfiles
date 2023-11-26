ae276887abce4116bad200fb57b81928241cedfb
Yusaku Hieda <uiyiuyiyi@gmail.com>
2023-11-26 17:23:37 +0900

    fix neovim change leader key to space

--- .config/nvim/lua/pluginconfig/translate.lua
+++ .config/nvim/lua/pluginconfig/translate.lua
@@ -20,6 +20,6 @@ local mappings = {
 }
 
 for k, v in pairs(mappings) do
-	vim.keymap.set({ "n", "x" }, "mj" .. k, ("<Cmd>Translate JA -source=EN %s<CR><Esc>"):format(v))
-	vim.keymap.set({ "n", "x" }, "me" .. k, ("<Cmd>Translate EN -source=JA %s<CR><Esc>"):format(v))
+	vim.keymap.set({ "n", "x" }, "<leader>tj" .. k, ("<Cmd>Translate JA -source=EN %s<CR><Esc>"):format(v))
+	vim.keymap.set({ "n", "x" }, "<leader>te" .. k, ("<Cmd>Translate EN -source=JA %s<CR><Esc>"):format(v))
 end
