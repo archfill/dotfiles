9954537ddb53be46b695ac9edb330d0f4844beee
Yusaku Hieda <uiyiuyiyi@gmail.com>
2024-03-01 23:03:12 +0900

    Added function to fzf and cd ghq list to zsh

--- .config/zsh/zshrc/functions.zsh
+++ .config/zsh/zshrc/functions.zsh
@@ -18,3 +18,14 @@ function select-history() {
   BUFFER=$(history -n -r 1 | fzf --no-sort +m --query "$LBUFFER" --prompt="History > ")
   CURSOR=$#BUFFER
 }
+
+function dotfiles() {
+  cd ~/dotfiles
+}
+
+function g() {
+  local src=$(ghq list | fzf --preview "ls -laTp $(ghq root)/{} | tail -n+4 | awk '{print \$9\"/\"\$6\"/\"\$7 \" \" \$10}'")
+  if [ -n "$src" ]; then
+    cd $(ghq root)/$src
+  fi
+}
