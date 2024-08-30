def25f2bbbe66ce5079c1ffe00709405f5139748
Yusaku Hieda <uiyiuyiyi@gmail.com>
2024-08-30 21:48:34 +0900

    fix termux

--- bin/termux/init.sh
+++ bin/termux/init.sh
@@ -3,6 +3,26 @@
 echo "--- setting start ---"
 
 mkdir -p ${HOME}/.config
+mkdir -p ${HOME}/git
+
+mkdir -p ${HOME}/git/termux
+mkdir -p ${HOME}/.termux
+# color
+# ${HOME}/.termux/colors.properties
+git clone https://github.com/Iamafnan/termux-tokyonight.git ${HOME}/git/termux/termux-tokyonight
+cp ${HOME}/git/termux/termux-tokyonight/colorschemes/tokyonight-night.properties ${HOME}/.termux/colors.properties
+
+# font
+# ${HOME}/.termux/font.ttf
+HACKGEN_VERSION=v2.9.0
+temp_dir=`pwd`
+mkdir -p ${HOME}/temp
+cd ${HOME}/temp
+curl -OL https://github.com/yuru7/HackGen/releases/download/${HACKGEN_VERSION}/HackGen_NF_${HACKGEN_VERSION}.zip
+unzip HackGen_NF_${HACKGEN_VERSION}.zip
+mv HackGen_NF_${HACKGEN_VERSION}/HackGenConsoleNF-Regular.ttf ${HOME}/.termux/font.ttf
+cd $temp_dir
+termux-reload-settings
 
 # install
 echo "--- apps install setup start ---"
