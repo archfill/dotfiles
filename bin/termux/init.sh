50279223cbf90bb55cc7ceb32f984f05366158b1
Yusaku Hieda <uiyiuyiyi@gmail.com>
2024-08-31 01:47:25 +0900

    fix termux

--- bin/termux/init.sh
+++ bin/termux/init.sh
@@ -40,8 +40,8 @@ echo "--- link setup finish ---"
 # apps
 echo "-- apps setup start ---"
 bash ${HOME}/dotfiles/bin/apps/zinit.sh
-# bash ${HOME}/dotfiles/bin/apps/volta.sh
-bash ${HOME}/dotfiles/bin/apps/nvm.sh
+bash ${HOME}/dotfiles/bin/apps/volta.sh
+# bash ${HOME}/dotfiles/bin/apps/nvm.sh
 bash ${HOME}/dotfiles/bin/apps/pyenv.sh
 echo "-- apps setup finish ---"
 
