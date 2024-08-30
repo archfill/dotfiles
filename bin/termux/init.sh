442f142c676ddedc05d49d8801a42c5bfa2b4486
Yusaku Hieda <uiyiuyiyi@gmail.com>
2024-08-30 21:16:59 +0900

    fix termux config

--- bin/termux/init.sh
+++ bin/termux/init.sh
@@ -5,6 +5,13 @@ echo "--- setting start ---"
 mkdir -p ${HOME}/.config
 
 # link
-echo "link setup start."
+echo "--- link setup start ---"
 bash ${HOME}/dotfiles/bin/termux/link.sh
-echo "link setup finish."
+echo "--- link setup finish ---"
+
+# apps
+echo "-- apps setup start ---"
+bash ${HOME}/dotfiles/bin/apps/zinit.sh
+bash ${HOME}/dotfiles/bin/apps/volta.sh
+bash ${HOME}/dotfiles/bin/apps/pyenv.sh
+echo "-- apps setup finish ---"
