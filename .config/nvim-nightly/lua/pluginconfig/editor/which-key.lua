94e94582d8a4ade8ba265450485b0568568faf68
Yusaku Hieda <uiyiuyiyi@gmail.com>
2025-06-15 00:29:17 +0900

    fix neovim

--- .config/nvim-nightly/lua/pluginconfig/editor/which-key.lua
+++
@@ -1,8 +0,0 @@
--- vim.o.timeout = true
--- vim.o.timeoutlen = 300
-require("which-key").setup({
-	triggers = "auto",
-	-- triggers = { "<leader>" },
-})
-
-vim.api.nvim_set_keymap("n", ",<CR>", "<Cmd>WhichKey ,<CR>", { noremap = true })
--- .config/nvim-stable/lua/colorscheme.lua
+++
@@ -1,8 +0,0 @@
--- Terminal Colors
-vim.opt.termguicolors = true
-
--- Tokyo Night Colorscheme
-vim.cmd.colorscheme("tokyonight")
-
--- Custom Highlights
-vim.cmd("hi Comment gui=NONE")
