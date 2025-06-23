7f8a40949b943b73a49b1df7608a0ca072c57d59
archfill <uiyiuyiyi@gmail.com>
2025-06-24 01:39:57 +0900

    fix neovim

--- .config/nvim/lua/plugins/tools/neo-tree.lua
+++ .config/nvim/lua/plugins/tools/neo-tree.lua
@@ -172,8 +172,10 @@ return {
               --".null-ls_*",
             },
           },
-          follow_current_file = true, -- This will find and focus the file in the active buffer every
-          -- time the current file is changed while the tree is open.
+          follow_current_file = {
+            enabled = true, -- This will find and focus the file in the active buffer every
+            -- time the current file is changed while the tree is open.
+          },
           group_empty_dirs = false, -- when true, empty folders will be grouped together
           hijack_netrw_behavior = "open_default", -- netrw hijack behavior
           use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
@@ -195,8 +197,10 @@ return {
           }
         },
         buffers = {
-          follow_current_file = true, -- This will find and focus the file in the active buffer every
-          -- time the current file is changed while the tree is open.
+          follow_current_file = {
+            enabled = true, -- This will find and focus the file in the active buffer every
+            -- time the current file is changed while the tree is open.
+          },
           group_empty_dirs = true, -- when true, empty folders will be grouped together
           show_unloaded = true,
           window = {
